28b5bcef3768ae58126740c843ddfa58e60536d2
Matheus-OAMK <t2fema00@students.oamk.fi>
2025-05-16 20:08:36 +0300

    refactor: Update functions with database interaction to use `AsyncDatabaseHandler`

--- src/db/document.py
+++ src/db/document.py
@@ -1,8 +1,9 @@
 import sys
 from pathlib import Path
-from typing import Any
+from typing import Any, Optional
+from uuid import UUID
 
-from src.db.db_handler import DatabaseHandler
+from psycopg import AsyncConnection
 
 # Add parent directory to Python path
 sys.path.append(str(Path(__file__).parents[2].resolve()))
@@ -13,14 +14,14 @@ from src.lib.logger import get_logger
 logger = get_logger(__name__)
 
 
-def get_document_by_name(
-    doc_name: str, db_handler: DatabaseHandler
+async def get_document_by_name(
+    doc_name: str, db_conn: AsyncConnection
 ) -> dict[str, Any] | None:
     """Retrieve a document from the database by its name.
 
     Args:
         doc_name: The unique name of the document to retrieve.
-        db_handler: Database connection handler instance.
+        db_conn: Database connection instance.
 
     Returns:
         Dictionary containing document data if found, None if no document exists.
@@ -38,10 +39,14 @@ def get_document_by_name(
     try:
         # Check if the document exists in the database
         query = "SELECT * FROM document WHERE name = %s"
-        result = db_handler.execute_query(
-            query=query, parameters=(doc_name,), fetch=True
+        cur = await db_conn.execute(
+            query=query,
+            params=(doc_name,),
         )
-        doc = result[0] if result else None
+
+        result = await cur.fetchone()
+
+        doc = result if result else None
         if not doc:
             return None
 
@@ -53,3 +58,38 @@ def get_document_by_name(
             exc_info=True,
         )
         raise ValueError(f"Error retrieving document: {e}")
+
+
+async def get_document_id_by_name(
+    db_conn: AsyncConnection,
+    document_name: str,
+) -> Optional[UUID]:
+    """
+    Get document ID from the database by document name.
+
+    Args:
+        db: Database handler instance
+        document_name: Name of the document
+
+    Returns:
+        UUID of the document if found, None otherwise
+    """
+    try:
+        query = "SELECT id FROM document WHERE name = %s"
+        parameters = (document_name,)
+
+        cur = await db_conn.execute(
+            query=query,
+            params=parameters,
+        )
+
+        result = await cur.fetchone()
+
+        if not result:
+            logger.warning(f"Document with name '{document_name}' not found")
+            return None
+
+        return result["id"]
+    except Exception as e:
+        logger.error(f"Error retrieving document ID: {e}")
+        return None
